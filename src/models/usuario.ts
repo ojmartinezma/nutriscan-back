8926b0169c6ed58d5eb66ffa4bc68e54e54bdfdd
OSCAR MARTINEZ <ojmartinezma@unal.edu.co>
2024-04-22 10:10:44 -0500

    Cambios validacion usuario

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -8,23 +8,48 @@ export const Usuario = sequelize.define('Usuario',{
     },
     nombre : {
         type: DataTypes.STRING,
+            validate:{
+                isAlphanumeric: true,
+            }
     },
     fechaSuscripcion : {
-        type: DataTypes.DATE
+        type: DataTypes.DATE,
+        validate: {
+            isDate: true, 
+            isAfter: "2023-04-01",
+        }
     },
     fechaDeNacimiento : {
         type: DataTypes.DATEONLY
     },
     altura : {
-        type : DataTypes.INTEGER
+        type : DataTypes.INTEGER,
+        validate: {
+            min: 50,
+            isInt: true,
+        }
     },
     peso : {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        validate: {
+            isInt: true,
+            min: 15
+        }
     },
     telefono : {
-        type: DataTypes.STRING
+        type: DataTypes.STRING,
+        validate: {
+            is: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/i
+        }
     },
     correo : {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+            isEmail: true, 
+        }
+    },
+    foto : {
         type: DataTypes.STRING
     }
 });
