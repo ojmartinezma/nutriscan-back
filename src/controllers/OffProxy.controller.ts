a4093e70fb9df66747ff8f5826faff022a47e27e
Juancho Rodriguez <juancho.chiquiza12@gmail.com>
2024-06-05 00:14:27 -0500

    added offimageupload proxy

---
+++ src/controllers/OffProxy.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+
+export const uploadOffImage = async (req:Request, res: Response) => {
+  try {
+    const { code, imagefield } = req.body;
+    const file = req.file;
+
+    console.log("uploadOffImage", {code, imagefield, file});
+
+    if(file){
+
+      const formdata = new FormData();
+      formdata.append("user_id", "jurodriguezch");
+      formdata.append("password", "OpenFoodFactsNutriScan");
+      formdata.append("code", code);
+      formdata.append("imagefield", imagefield);
+      formdata.append(`imgupload_${imagefield}`, new Blob([file.buffer]), file.filename);
+
+      const requestOptions = {
+        method: "POST",
+        body: formdata
+      };
+  
+      const response = await fetch("https://world.openfoodfacts.net/cgi/product_image_upload.pl", requestOptions)
+      const result = await response.json();
+
+      console.log("success");
+      res.status(200).json(result);
+    }else{
+      throw new Error("field offimg return undefined")
+    }
+
+  } catch(error: unknown){
+    if (error instanceof Error) {
+      res.status(500).json({"message":error.message})
+    }
+  }
+  
+}
