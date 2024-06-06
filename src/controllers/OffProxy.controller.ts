b063059648532ee9c2c802573fabe2804c605834
Juancho Rodriguez <juancho.chiquiza12@gmail.com>
2024-06-05 20:46:37 -0500

    fixed node-fetch import, fixed formdata metadata

--- src/controllers/OffProxy.controller.ts
+++ src/controllers/OffProxy.controller.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from "express";
 import FormData from "form-data";
-const fetch = require('node-fetch');
+
+import("node-fetch").then(res => {
+  if(!globalThis.fetch){
+    // @ts-expect-error
+    globalThis.fetch = res.default;
+    // @ts-expect-error
+    globalThis.Headers = res.Headers;
+    // @ts-expect-error
+    globalThis.Request = res.Request
+    // @ts-expect-error
+    globalThis.Response = res.Response;
+    globalThis.Blob = res.Blob;
+  }
+})
 
 export const uploadOffImage = async (req:Request, res: Response) => {
   try {
@@ -16,13 +29,19 @@ export const uploadOffImage = async (req:Request, res: Response) => {
       formdata.append("password", "OpenFoodFactsNutriScan");
       formdata.append("code", code);
       formdata.append("imagefield", imagefield);
-      formdata.append(`imgupload_${imagefield}`, new Blob([file.buffer]), file.filename);
+      formdata.append(`imgupload_${imagefield}`, file.buffer, {
+        filename: file.filename,
+        filepath: file.path,
+        contentType: file.mimetype,
+        knownLength: file.size
+      });
 
       const requestOptions = {
         method: "POST",
         body: formdata
       };
-  
+
+    // @ts-expect-error
       const response = await fetch("https://world.openfoodfacts.net/cgi/product_image_upload.pl", requestOptions);
       const result = await response.json();
 
